92d9853d5f641bf314637e9324ee8befb1968262
Edward Thomson <ethomson@edwardthomson.com>
2018-04-20 13:43:52 +0100

    docurium: support for docs generator

---
+++ scripts/docurium.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# since docurium commits the doc changes, these need to be configured
+git config user.name "Edward Thomson"
+git config user.email "ethomson@edwardthomson.com"
+
+cm doc api.docurium
